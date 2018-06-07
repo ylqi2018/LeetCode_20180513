b07a16f4c45059946a1d3aafdfaea912c26ad347
ylqi007 <yunlongqi@my.unt.edu>
2018-06-07 11:14:47 -0500

    Top 100 liked, Stack

--- src/Stack/_394_DecodeString.java
+++ src/Stack/_394_DecodeString.java
@@ -27,4 +27,43 @@ public class _394_DecodeString {
         }
         return sb.toString();
     }
+    
+    /*
+     * Method 2
+     */
+    public String decodeString1(String s) {
+        if(s.length() == 0) {
+            return "";
+        }
+        StringBuilder sb = new StringBuilder();
+        for(int i=0; i<s.length(); i++) {
+            char c = s.charAt(i);
+            if(Character.isDigit(c)) {
+                int digit_begin = i;
+                while(s.charAt(i) != '[') {
+                    i++;        // to find the index of the first '['
+                }
+                int num = Integer.valueOf(s.substring(digit_begin, i)); // s(digit_begin)-s(i-1)
+                int count = 1;  // one '[' now, count the number of '[' and ']'
+                int str_begin = i + 1;  // the begin index of str
+                i++;        // because the '[' at index i has already be counted
+                while(count != 0) {
+                    if(s.charAt(i) == '[') {
+                        count++;
+                    } else if(s.charAt(i) == ']') {
+                        count--;
+                    }
+                    i++;
+                }
+                i--;    // i is added 1 additional 1 time, it will out of the length of string s
+                String str = decodeString(s.substring(str_begin, i));    // the substring int the first pair []
+                for(int j=0; j<num; j++) {
+                    sb.append(str);
+                }                
+            } else {
+                sb.append(c);
+            }
+        }
+        return sb.toString();
+    }
 }
